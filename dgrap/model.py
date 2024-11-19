61a6c18b8090f09d8ca66e98cc47cf0cb8d24102
CarloSantoro21 <alessandrosantoro212121@gmail.com>
2024-11-19 15:01:14 -0600

    corrección dgrap

--- dgrap/model.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from uuid import UUID
-from pydantic import BaseModel
-
-class User(BaseModel):
-    uid: str | None
-    username: str
-    email: str
-    created_at: datetime | None
